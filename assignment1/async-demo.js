02c33ecc3263b21bc0b267daa3fa0a1a187458fb
judy99 <julia.kurianova@gmail.com>
2025-11-17 18:28:37 -0500

    first assignment

--- assignment1/async-demo.js
+++ assignment1/async-demo.js
@@ -1,16 +1,75 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const fsPromise = require("fs/promises");
+const path = require("path");
 
-
-// Write a sample file for demonstration
+const filePath = path.join(__dirname, "./sample-files/sample.txt");
+const content = "Hello, async world!";
+fs.writeFileSync(filePath, content, "utf-8");
 
 // 1. Callback style
+fs.readFile(filePath, "utf-8", (err, data) => {
+  if (err) {
+    console.log("Error while reading: ", err);
+  } else {
+    console.log("Callback read: ", data);
+  }
+});
+// Callback hell example (test and leave it in comments):
+// open file
+// fs.open(filePath, (err, fileHandle) => {
+//   if (err) {
+//     console.log("File open failed: ", err.message);
+//   } else {
+//     console.log("File open succeeded.  The file handle is: ", fileHandle);
+//     // read file
+//     fs.readFile(fileHandle, "utf-8", (err, data) => {
+//       if (err) {
+//         console.log("Error while reading: ", err);
+//       } else {
+//         // log file info
+//         console.log("Initial file: ", data);
+//         // write to file
+//         fs.appendFile(filePath, " Hello again!", (err) => {
+//           if (err) {
+//             console.log("Error while writing: ", err);
+//           } else {
+//             console.log("Successfully wrote data");
+//             // read again
+//             fs.readFile(filePath, "utf-8", (err, data) => {
+//               if (err) {
+//                 console.log("err while reading - 2: ", err);
+//               }
+//               // log file info after adding
+//               console.log("Updated file: ", data);
+//             });
+//           }
+//         });
+//       }
+//     });
+//   }
+// });
 
+// 2. Promise style
+const doFileOperations = async () => {
+  try {
+    fsPromise.readFile(filePath, "utf-8").then((data) => {
+      console.log("Promise read: ", data);
+    });
+  } catch (err) {
+    console.log("An error occurred.", err);
+  }
+};
 
-  // Callback hell example (test and leave it in comments):
+doFileOperations();
 
+// 3. Async/Await style
+const doAsyncAwait = async () => {
+  try {
+    const filehandle = await fsPromise.readFile(filePath, "utf-8");
+    console.log("Async/Await read: ", filehandle);
+  } catch (err) {
+    console.log("An error occurred.", err);
+  }
+};
 
-  // 2. Promise style
-
-
-      // 3. Async/Await style
+doAsyncAwait();
--- assignment1/core-modules-demo.js
+++ assignment1/core-modules-demo.js
@@ -1,18 +1,65 @@
-const os = require('os');
-const path = require('path');
-const fs = require('fs');
+const os = require("os");
+const path = require("path");
+const fs = require("fs");
+const fsPromises = require("fs/promises");
 
-const sampleFilesDir = path.join(__dirname, 'sample-files');
+const sampleFilesDir = path.join(__dirname, "sample-files");
 if (!fs.existsSync(sampleFilesDir)) {
   fs.mkdirSync(sampleFilesDir, { recursive: true });
 }
 
+// Platform: darwin
+// CPU: Intel(R) Core(TM) i7-9750H CPU @ 2.60GHz
+// Total Memory: 17179869184
 // OS module
-
+console.log("Platform:", os.platform());
+console.log("CPU:", os.cpus()[0].model);
+console.log("Total Memory:", os.totalmem());
 
 // Path module
-
+// Joined path: /path/to/sample-files/folder/file.txt
+console.log(
+  "Joined path:",
+  path.join("path/to/sample-files", "folder/file.txt")
+);
 // fs.promises API
+// fs.promises read: Hello from fs.promises!
+const filePath = path.join(__dirname, "./sample-files/demo.txt");
+const content = "Hello from fs.promises!";
+const readWriteFunction = async () => {
+  try {
+    await fsPromises.writeFile(filePath, content, "utf-8");
+    const result = await fsPromises.readFile(filePath, "utf-8");
+    console.log("fs.promises read:", result);
+  } catch (error) {
+    console.log("error", error);
+  }
+};
 
-
+readWriteFunction();
 // Streams for large files- log first 40 chars of each chunk
+// Read chunk: This is a line in a large file...
+// Finished reading large file with streams.
+const largeFilePath = path.join(__dirname, "./sample-files/largefile.txt");
+const contentLine = "This is a line in a large file...\n";
+const readWriteLargeFileFunction = async () => {
+  try {
+    for (let i = 0; i < 100; i++) {
+      await fsPromises.appendFile(largeFilePath, contentLine, "utf-8");
+    }
+    const readStream = fs.createReadStream(largeFilePath, {
+      encoding: "utf-8",
+      highWaterMark: 1024,
+    });
+    readStream.on("data", (chunk) => {
+      console.log("Read chunk:\n", chunk);
+    });
+
+    readStream.on("end", () => {
+      console.log("Finished reading large file with streams.");
+    });
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+readWriteLargeFileFunction();
