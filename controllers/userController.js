f2372370863eeefb3afc7337105d46889c3e3912
judy99 <julia.kurianova@gmail.com>
2026-01-06 12:28:20 -0500

    Add user show method

--- controllers/userController.js
+++ controllers/userController.js
@@ -98,4 +98,39 @@ function logoff(req, res) {
   return res.sendStatus(StatusCodes.OK);
 }
 
-module.exports = { register, logon, logoff };
+async function show(req, res) {
+  const userId = parseInt(req.params.id);
+
+  if (isNaN(userId)) {
+    return res.status(400).json({ error: "Invalid user ID" });
+  }
+
+  const user = await prisma.user.findUnique({
+    where: { id: userId },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      createdAt: true,
+      Task: {
+        where: { isCompleted: false },
+        select: {
+          id: true,
+          title: true,
+          priority: true,
+          createdAt: true,
+        },
+        orderBy: { createdAt: "desc" },
+        take: 5,
+      },
+    },
+  });
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  res.status(200).json(user);
+}
+
+module.exports = { register, logon, logoff, show };
