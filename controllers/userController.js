6d81f71131b11f64233978b11683e7c1c2916151
judy99 <julia.kurianova@gmail.com>
2026-01-27 18:46:05 -0500

    change sameSite

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,7 +11,7 @@ const cookieFlags = (req) => {
   return {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production", // only when HTTPS is available
-    sameSite: "Strict",
+    sameSite: "none",
   };
 };
 
